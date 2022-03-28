b8d8da1cd794cf79730c49cadea9f52d70651085
Hussain Jawad <45372216+HusJAW@users.noreply.github.com>
2022-03-28 06:18:27 +0100

    style update

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 let range = document.querySelector("input");
 
-let min = document.querySelector(".min");
-let sec = document.querySelector(".sec");
+let minutes = document.querySelector(".min");
+let seconds = document.querySelector(".sec");
 
 range.addEventListener("input", (e) => {
   // console.log(range.value)
-  min.innerText = range.value;
+  minutes.innerText = range.value;
 });
 
-let secCounter = 0;
-let minCounter = 0;
+let secondsCounter = 0;
+let minutesCounter = 0;
 
 function countDown() {
   console.log("countDown");
